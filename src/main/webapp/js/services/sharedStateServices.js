fee07216c953fcd5f5cd75b8c5674553b1f25be7
Carl Schroedl <cschroedl@usgs.gov>
2013-12-09 13:19:48 -0600

    Implemented dependent non-persistent state restoration mock.

--- src/main/webapp/js/services/sharedStateServices.js
+++ src/main/webapp/js/services/sharedStateServices.js
@@ -2,14 +2,16 @@
 (function(){
 var sharedStateServices = angular.module('stateDemoApp.sharedStateServices', []);
 
-var storedState = {
+//enable sugarjs instance methods
+var storedState = Object.extended({
     _version: 0.1,
     _clientState: {
         name: undefined,
         params: {}
         
     }
-};
+});
+
 //this factory provides access to the state that can be Stored to the server and shared between controllers
 sharedStateServices.factory('StoredState', 
     function(){
@@ -17,33 +19,35 @@ sharedStateServices.factory('StoredState',
     }
 );
 
-//support sugarjs methods
+//enable sugarjs instance methods
 var commonState = Object.extended();
-var listeningToStoredState = false;
 
-//map of key in StoredState to listener function as called by $rootScope.$watch()
-var storedStateListeners = {
-    favoriteColor : function(newValue, oldValue){
-        if(newValue === 'blue'){
-            commonState.favoriteColorDependent = 'BLUE DATA SET';
-        }
-        
-    }
-};
 //this factory provides access to the state that is NOT stored to the server, but that
 //can be shared between controllers
 //generally, the commonState should NOT be written to by controllers, rather it should
 //be written to by listeners on the StoredState
-sharedStateServices.factory('CommonState', ['$rootScope',
-    function($rootScope){
-        if(!listeningToStoredState){
-            angular.forEach(storedStateListeners, function(listener, propertyToObserve){
-
-               $rootScope.$watch('StoredState['+propertyToObserve+']', listener); 
-            });
-            listeningToStoredState = true;
-        }
+sharedStateServices.factory('CommonState', [
+    function(){
         return commonState;
     }
 ]);
+
+storedState.watch('favoriteColor', function(prop, oldValue, newValue){
+    //this would normally be replaced by ajax calls whose callbacks update
+    //commonState
+    if(newValue === 'blue'){
+        commonState.favoriteColorDependent = 'BlueDataSet';
+    }
+    else if(newValue === 'yellow'){
+        commonState.favoriteColorDependent = 'YellowDataSet';
+    }
+    else{
+        commonState.favoriteColorDependent = undefined;
+    }
+    
+    //be EXTRA CERTAIN to return a value from the watch or else the property
+    //will never be written to
+    return newValue;
+});
+
 }());
